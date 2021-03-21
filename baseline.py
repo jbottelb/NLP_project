d01e07e586c761978e6d978b787f3c3b8761beb4
jbottelb <jbottelb@nd.edu>
2021-03-21 12:48:21 -0400

    initial imports

---
+++ baseline.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+This is the baseline solution. The goal is to beat this with better NLP
+solutions.
+
+Current method: Random responces OR choose a random respose / comment
+'''
+
+class Random_Response:
+    def __init__(self, data):
+        pass
---
+++ handler.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+Program handles the different models and reports evaluation metric success
+'''
+
+import argparse
+import baseline
+
+
+def main():
+    pass
+
+
+
+if __name__  == "__main__":
+    main()
