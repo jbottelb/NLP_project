60789cb3034a601410ca09b1379c6783d3d56d1c
jbottelb <jbottelb@nd.edu>
2021-04-12 23:03:14 -0400

    subreddit module

--- baseline.py
+++ baseline.py
@@ -7,6 +7,22 @@ solutions.
 Current method: Random responces OR choose a random respose / comment
 '''
 
+import sys, subreddit as s
+
 class Random_Response:
+    '''
+    A random response model: it will randomly select a respose from the data
+    '''
     def __init__(self, data):
         pass
+
+
+if __name__ == "__main__":
+    _, input_file = sys.argv
+
+    comments = []
+
+    sub = s.Subreddit(input_file)
+
+    print(sub.posts[0]["comments"][1])
+    print(sub)
